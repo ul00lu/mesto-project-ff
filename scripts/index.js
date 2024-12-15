ed6e0c5ef01de36235f0aeda75b3f475bfe75023
ul00lu <serafim.kara08@mail.ru>
2024-12-15 17:29:25 +0600

    Добавление и удаление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-
+const list = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
+function addCard(name, link) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__title').textContent = name;
+	cardElement.querySelector('.card__image').src = link;
 
-// @todo: Функция удаления карточки
+	cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
 
-// @todo: Вывести карточки на страницу
+	list.append(cardElement);
+}
+
+function removeCard(evt) {
+	evt.target.closest('.card').remove();
+}
+
+// for (let i = 0; i < initialCards.length; i += 1) {
+// 	addCard(initialCards[i].name, initialCards[i].link);
+// }
+initialCards.forEach(element => {
+	addCard(element.name, element.link);
+});
